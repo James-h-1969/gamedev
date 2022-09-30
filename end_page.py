98456004b31686deb943179d6ed926cfc85ecdc3
annabel <annabel.yw.chen@gmail.com>
2022-10-01 00:18:00 +1000

    Sam's collision code added to main.py

--- end_page.py
+++ end_page.py
@@ -3,12 +3,15 @@ import sys
 import os
 from constants import *
 
-
-# LOAD IMAGES
-background = pygame.image.load(os.path.join('img', 'end-background.png'))
-background = background.convert()
+pygame.init()
 WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+# LOAD IMAGES
+
+background = pygame.image.load(os.path.join('img', 'end-background.png'))
+WINDOW.blit(background, (0,0))
+
+background = background.convert()
 class EndPage():
     def __init__(self):
         self.running = True
@@ -18,13 +21,16 @@ class EndPage():
     def endPage_main(self, WINDOW):
         while self.running:
             self.clock.tick(FPS)
+
+            #displaying shit
+            pygame.display.flip()
+
+            # quitting
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            WINDOW.blit(background, (0,0))
-            pygame.display.flip()
-            pygame.display.update()
+            
                 
 
 trial = EndPage()
