901f66c8b9b9774a20248ed90096638964283cc8
James Hocking <jameshocking542@gmail.com>
2022-09-30 22:14:55 +1000

    end_page

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from constants import *
 from start_page import StartPage
 from ball import Ball
 from levels import Levels
+from end_page import *
 pygame.font.init()
 
 WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     start_page = StartPage()
-    levels = Levels()
+    level = Levels()
+    end_page = EndPage()
     
     start_page.startPage_main(WINDOW) 
-    levels.levels_main(WINDOW)
+    level.levels_main(WINDOW)
+    end_page.endPage_main(WINDOW)
+
+
     pygame.quit()
 
 if __name__ == "__main__":
