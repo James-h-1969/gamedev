5f0548a08a7f36fad4d2c3d243d31259b3452882
James Hocking <jameshocking542@gmail.com>
2022-09-30 19:35:17 +1000

    setup

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import pygame
+from constants import *
+from start_page import StartPage
+from ball import Ball
+pygame.font.init()
 
+WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def main():
+    while StartPage.running:
+        StartPage.startPage() 
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ start_page.py
@@ -0,0 +1,8 @@
+import pygame
+
+class StartPage():
+    def __init__(self):
+        StartPage.running = True
+    
+    def startPage():
+        pass
